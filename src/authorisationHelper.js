7f45c7ce96f9f47854451678b06b0f952a8134fc
Min Park <minpark@built.com.au>
2019-08-21 10:47:16 +1000

    1.8.0: Add userInfo in req object to map with ConformID user info.

--- src/authorisationHelper.js
+++ src/authorisationHelper.js
@@ -1,6 +1,7 @@
 const R = require('ramda');
 const { builtErrorCodes } = require('./errorHandler');
-const { throwErrorIfFieldNotProvided, throwCustomErrorIfFalseCondition } = require('./validationHelper');
+const { throwErrorIfFieldNotProvided, throwErrorIfNoObjectExists, throwCustomErrorIfFalseCondition } = require('./validationHelper');
+const requestHelper = require('./requestHelper');
 const { createToken, verifyToken } = require('./jwtHelper');
 
 const INTERNAL_SERVICE = 'internal_service';
@@ -9,16 +10,21 @@ const config = {
     appName: null,
     userSecretKey: null,
     internalServiceSecretKey: null,
+    conformIdRootUrl: null,
 };
 
-const setup = ({ appName, userSecretKey, internalServiceSecretKey }) => {
+const setup = ({
+    appName, userSecretKey, internalServiceSecretKey, conformIdRootUrl,
+}) => {
     throwErrorIfFieldNotProvided(appName, 'appName');
     throwErrorIfFieldNotProvided(userSecretKey, 'userSecretKey');
     throwErrorIfFieldNotProvided(internalServiceSecretKey, 'internalServiceSecretKey');
+    throwErrorIfFieldNotProvided(conformIdRootUrl, 'conformIdRootUrl');
 
     config.appName = appName;
     config.userSecretKey = userSecretKey;
     config.internalServiceSecretKey = internalServiceSecretKey;
+    config.conformIdRootUrl = conformIdRootUrl;
 };
 
 const validateSetup = () => {
@@ -65,17 +71,61 @@ const getUserRoles = async (token) => {
     };
 };
 
+// Obsolete
+// TODO: Remove
 const getRoles = (req) => {
     const serviceToken = req.header('x-internal-service-jwt');
     const userToken = req.header('x-user-jwt');
     return serviceToken ? getInternalServiceRole(serviceToken) : getUserRoles(userToken);
 };
 
+const getConformIdUserInfo = async (token) => {
+    const decoded = await verifyToken(token, config.userSecretKey);
+    const userInfo = decoded.userjwtstring ? JSON.parse(decoded.userjwtstring) : decoded;
+    throwErrorIfNoObjectExists(userInfo, 'user');
+    // TODO: Update the path as per Apigee change
+    const email = R.path(['registry', 'email'])(userInfo);
+    throwErrorIfNoObjectExists(email, 'user email');
+
+    const endpoint = `${config.conformIdRootUrl}/users/${email}/roles`;
+    const { error, data } = await requestHelper.get(endpoint, null, null, true);
+    if (error) {
+        throwCustomErrorIfFalseCondition(false, error.code, error.field, error.details);
+    }
+    return data;
+};
+
+const getUserInfo = async (req) => {
+    const serviceToken = req.header('x-internal-service-jwt');
+    const userToken = req.header('x-user-jwt');
+    return serviceToken
+        ? getInternalServiceRole(serviceToken)
+        : getConformIdUserInfo(userToken);
+};
+
+// Obsolete
+// TODO: Remove
 const hasRole = (roleData, roleName, entity = null, entityId = null) => {
     validateSetup();
     return roleData && roleData.some(i => i.name === roleName
         && i.entity === entity
         && i.entityId === entityId);
+};
+
+const hasRoleNew = (userOrgs, organisationId, roleName, entity = null, entityId = null) => {
+    validateSetup();
+
+    const userOrg = userOrgs.find(i => i.organisationId === organisationId);
+    if (!userOrg) { return false; }
+
+    const checkEntity = (targetEntityId, entities) => {
+        if (!targetEntityId) { return true; }
+        return entities && entities.include(targetEntityId);
+    };
+
+    return userOrg.roles && userOrg.roles.some(i => i.name === roleName
+        && i.entityType === entity
+        && checkEntity(entityId, i.entities));
 };
 
 const validateAuthResult = (authorised, next) => {
@@ -89,7 +139,8 @@ const authorise = (authFunc) => {
     return async (req, res, next) => {
         let result = null;
         try {
-            req.user = await getRoles(req);
+            req.user = await getRoles(req); // TODO: Obsolete
+            req.userInfo = await getUserInfo(req);
             result = authFunc(req);
         } catch (error) {
             return next(error);
@@ -113,6 +164,7 @@ const authorise = (authFunc) => {
 module.exports = {
     setup,
     hasRole,
+    hasRoleNew,
     authorise,
     generateInternalServiceToken,
 };
